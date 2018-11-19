0534c6e4ceb5568c7cb322b1b7ba60f00ddcd4fe
Robert Freiberger <rfreiberger@gmail.com>
2018-11-19 09:55:33 -0800

    corrected line break

--- SQL/ability_score.sql
+++ SQL/ability_score.sql
@@ -1,2 +1,1 @@
-CREATE TABLE ability_score ( id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY, ability INT(4), ability_mod INT(4
-), wizard_spells_known VARCHAR(25), max_spell_level INT(4));
+CREATE TABLE ability_score ( id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY, ability INT(4), ability_mod INT(4), wizard_spells_known VARCHAR(25), max_spell_level INT(4));
